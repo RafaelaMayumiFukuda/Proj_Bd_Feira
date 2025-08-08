c33afa152bc56b17b637b4799a9fc9d801ed653d
Rafaela Mayumi Wada Fukuda <125597771+RafaelaMayumiFukuda@users.noreply.github.com>
2025-08-08 18:15:03 -0300

    Add files via upload

--- feira.sql
+++ feira.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 28-Jul-2025 às 21:53
--- Versão do servidor: 10.4.22-MariaDB
--- versão do PHP: 8.1.2
+-- Tempo de geração: 08/08/2025 às 23:13
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,18 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `ods_projeto`
+-- Estrutura para tabela `ods_projeto`
 --
 
 CREATE TABLE `ods_projeto` (
   `id_ods` int(11) NOT NULL,
   `id_projetos` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `ods_projeto`
+--
+
+INSERT INTO `ods_projeto` (`id_ods`, `id_projetos`) VALUES
+(7, 1),
+(11, 1),
+(2, 2),
+(14, 2),
+(15, 2),
+(2, 3);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tbl_alunos`
+-- Estrutura para tabela `tbl_alunos`
 --
 
 CREATE TABLE `tbl_alunos` (
@@ -44,23 +56,46 @@ CREATE TABLE `tbl_alunos` (
   `nome` varchar(100) NOT NULL,
   `serie` enum('1','2','3') NOT NULL,
   `curso` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tbl_ods`
+-- Estrutura para tabela `tbl_ods`
 --
 
 CREATE TABLE `tbl_ods` (
   `id_ods` int(11) NOT NULL,
   `ods` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tbl_ods`
+--
+
+INSERT INTO `tbl_ods` (`id_ods`, `ods`) VALUES
+(1, 'Erradicação da pobreza'),
+(2, 'Fome zero e agricultura sustentável'),
+(3, 'Saúde e bem-estar'),
+(4, 'Educação de qualidade'),
+(5, 'Igualdade de gênero'),
+(6, 'Água potável e saneamento'),
+(7, 'Energia limpa e acessível'),
+(8, 'Trabalho decente e crescimento econômico'),
+(9, 'Indústria, inovação e infraestrutura'),
+(10, 'Redução das desigualdades'),
+(11, 'Cidades e comunidades sustentáveis'),
+(12, 'Consumo e produção responsáveis'),
+(13, 'Ação contra a mudança global do clima'),
+(14, 'Vida na água'),
+(15, 'Vida terrestre'),
+(16, 'Paz, justiça e instituições eficazes'),
+(17, 'Parcerias e meios de implementação');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tbl_projetos`
+-- Estrutura para tabela `tbl_projetos`
 --
 
 CREATE TABLE `tbl_projetos` (
@@ -73,10 +108,10 @@ CREATE TABLE `tbl_projetos` (
   `posicao_projeto` int(11) NOT NULL,
   `stand` varchar(3) NOT NULL,
   `prof_orientador` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Extraindo dados da tabela `tbl_projetos`
+-- Despejando dados para a tabela `tbl_projetos`
 --
 
 INSERT INTO `tbl_projetos` (`id_projetos`, `titulo_projeto`, `descricao_projeto`, `ods`, `bloco`, `sala`, `posicao_projeto`, `stand`, `prof_orientador`) VALUES
@@ -87,7 +122,7 @@ INSERT INTO `tbl_projetos` (`id_projetos`, `titulo_projeto`, `descricao_projeto`
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tbl_users`
+-- Estrutura para tabela `tbl_users`
 --
 
 CREATE TABLE `tbl_users` (
@@ -97,10 +132,10 @@ CREATE TABLE `tbl_users` (
   `email` varchar(100) NOT NULL,
   `senha` varchar(255) DEFAULT NULL,
   `data_nasc` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Extraindo dados da tabela `tbl_users`
+-- Despejando dados para a tabela `tbl_users`
 --
 
 INSERT INTO `tbl_users` (`id_users`, `is_admin`, `nome`, `email`, `senha`, `data_nasc`) VALUES
@@ -111,7 +146,7 @@ INSERT INTO `tbl_users` (`id_users`, `is_admin`, `nome`, `email`, `senha`, `data
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tb_creditos`
+-- Estrutura para tabela `tb_creditos`
 --
 
 CREATE TABLE `tb_creditos` (
@@ -121,12 +156,12 @@ CREATE TABLE `tb_creditos` (
   `foto_dev` varchar(128) NOT NULL,
   `linkedin_dev` varchar(255) DEFAULT NULL,
   `github_dev` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tb_feedback`
+-- Estrutura para tabela `tb_feedback`
 --
 
 CREATE TABLE `tb_feedback` (
@@ -135,23 +170,23 @@ CREATE TABLE `tb_feedback` (
   `nota` int(1) NOT NULL,
   `comentario` varchar(400) DEFAULT NULL,
   `data_envio` datetime DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tb_integrantes`
+-- Estrutura para tabela `tb_integrantes`
 --
 
 CREATE TABLE `tb_integrantes` (
   `id_projetos` int(11) NOT NULL,
   `id_aluno` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tb_votos`
+-- Estrutura para tabela `tb_votos`
 --
 
 CREATE TABLE `tb_votos` (
@@ -161,10 +196,10 @@ CREATE TABLE `tb_votos` (
   `coment_voto` varchar(200) DEFAULT NULL,
   `id_user` int(11) NOT NULL,
   `id_projetos` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Extraindo dados da tabela `tb_votos`
+-- Despejando dados para a tabela `tb_votos`
 --
 
 INSERT INTO `tb_votos` (`id_votos`, `dt_hora_voto`, `valor_voto`, `coment_voto`, `id_user`, `id_projetos`) VALUES
@@ -177,58 +212,58 @@ INSERT INTO `tb_votos` (`id_votos`, `dt_hora_voto`, `valor_voto`, `coment_voto`,
 --
 
 --
--- Índices para tabela `ods_projeto`
+-- Índices de tabela `ods_projeto`
 --
 ALTER TABLE `ods_projeto`
   ADD KEY `id_ods` (`id_ods`),
   ADD KEY `id_projetos` (`id_projetos`);
 
 --
--- Índices para tabela `tbl_alunos`
+-- Índices de tabela `tbl_alunos`
 --
 ALTER TABLE `tbl_alunos`
   ADD PRIMARY KEY (`id_aluno`);
 
 --
--- Índices para tabela `tbl_ods`
+-- Índices de tabela `tbl_ods`
 --
 ALTER TABLE `tbl_ods`
   ADD PRIMARY KEY (`id_ods`);
 
 --
--- Índices para tabela `tbl_projetos`
+-- Índices de tabela `tbl_projetos`
 --
 ALTER TABLE `tbl_projetos`
   ADD PRIMARY KEY (`id_projetos`);
 
 --
--- Índices para tabela `tbl_users`
+-- Índices de tabela `tbl_users`
 --
 ALTER TABLE `tbl_users`
   ADD PRIMARY KEY (`id_users`);
 
 --
--- Índices para tabela `tb_creditos`
+-- Índices de tabela `tb_creditos`
 --
 ALTER TABLE `tb_creditos`
   ADD PRIMARY KEY (`id_dev`) USING BTREE;
 
 --
--- Índices para tabela `tb_feedback`
+-- Índices de tabela `tb_feedback`
 --
 ALTER TABLE `tb_feedback`
   ADD PRIMARY KEY (`id_feedback`),
   ADD KEY `id_users` (`id_users`);
 
 --
--- Índices para tabela `tb_integrantes`
+-- Índices de tabela `tb_integrantes`
 --
 ALTER TABLE `tb_integrantes`
   ADD KEY `id_projetos_integrantes` (`id_projetos`),
   ADD KEY `id_alunos_integrantes` (`id_aluno`);
 
 --
--- Índices para tabela `tb_votos`
+-- Índices de tabela `tb_votos`
 --
 ALTER TABLE `tb_votos`
   ADD PRIMARY KEY (`id_votos`),
@@ -236,7 +271,7 @@ ALTER TABLE `tb_votos`
   ADD KEY `id_users` (`id_user`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
@@ -249,7 +284,7 @@ ALTER TABLE `tbl_alunos`
 -- AUTO_INCREMENT de tabela `tbl_ods`
 --
 ALTER TABLE `tbl_ods`
-  MODIFY `id_ods` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_ods` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de tabela `tbl_projetos`
@@ -282,31 +317,31 @@ ALTER TABLE `tb_votos`
   MODIFY `id_votos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Restrições para despejos de tabelas
+-- Restrições para tabelas despejadas
 --
 
 --
--- Limitadores para a tabela `ods_projeto`
+-- Restrições para tabelas `ods_projeto`
 --
 ALTER TABLE `ods_projeto`
   ADD CONSTRAINT `ods_projeto_ibfk_1` FOREIGN KEY (`id_ods`) REFERENCES `tbl_ods` (`id_ods`),
   ADD CONSTRAINT `ods_projeto_ibfk_2` FOREIGN KEY (`id_projetos`) REFERENCES `tbl_projetos` (`id_projetos`);
 
 --
--- Limitadores para a tabela `tb_feedback`
+-- Restrições para tabelas `tb_feedback`
 --
 ALTER TABLE `tb_feedback`
   ADD CONSTRAINT `tb_feedback_ibfk_1` FOREIGN KEY (`id_users`) REFERENCES `tbl_users` (`id_users`);
 
 --
--- Limitadores para a tabela `tb_integrantes`
+-- Restrições para tabelas `tb_integrantes`
 --
 ALTER TABLE `tb_integrantes`
   ADD CONSTRAINT `id_alunos_integrantes` FOREIGN KEY (`id_aluno`) REFERENCES `tbl_alunos` (`id_aluno`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `id_projetos_integrantes` FOREIGN KEY (`id_projetos`) REFERENCES `tbl_projetos` (`id_projetos`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Limitadores para a tabela `tb_votos`
+-- Restrições para tabelas `tb_votos`
 --
 ALTER TABLE `tb_votos`
   ADD CONSTRAINT `id_projetos` FOREIGN KEY (`id_projetos`) REFERENCES `tbl_projetos` (`id_projetos`) ON DELETE CASCADE ON UPDATE CASCADE,
